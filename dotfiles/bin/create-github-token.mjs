6d46c12a33ab22b9c1e5d340a24e87d7589c046d
SirWindfield <SirWindfield@users.noreply.github.com>
2021-05-12 03:32:25 +0200

    feat: add create-github-token script

---
+++ dotfiles/bin/create-github-token.mjs
@@ -0,0 +1,41 @@
+#!/usr/bin/env hs
+
+const scopes = [
+    "repo",
+    "repo:status",
+    "repo_deployment",
+    "public_repo",
+    "repo:invite",
+    "security_events",
+
+    "workflow",
+
+    "write:packages",
+    "read:packages",
+
+    "delete:packages",
+
+    "gist",
+
+    "notifications",
+
+    "delete_repo",
+]
+const questions = [
+    {
+        type: 'input',
+        name: 'description',
+        message: 'Name of the token',
+    },
+    {
+        type: 'autocomplete',
+        name: 'scopes',
+        message: 'Select access token scopes',
+        choices: scopes,
+        multiple: true,
+    },
+]
+
+const answers = await prompt(questions)
+const url = `https://github.com/settings/tokens/new?description=${answers['description']}&scopes=${answers['scopes'].join(',')}`
+await open(url)

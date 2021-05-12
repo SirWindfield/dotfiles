9733b186e5bc97065e1a96f2e6122da40a97ed77
SirWindfield <SirWindfield@users.noreply.github.com>
2021-05-12 03:56:15 +0200

    fix: protonvpn target filename on macos

--- dotfiles/install/macos/protonvpn.sh
+++ dotfiles/install/macos/protonvpn.sh
@@ -2,7 +2,7 @@
 filename="protonvpn.dmg"
 version="1.9.3"
 
-curl --proto '=https' --tlsv1.2 -sSfo "$file" "https://protonvpn.com/download/ProtonVPN_mac_v$version.dmg"
+curl --proto '=https' --tlsv1.2 -sSfo "$filename" "https://protonvpn.com/download/ProtonVPN_mac_v$version.dmg"
 
 # Mount the dmg file, copy over the .app directory, detach the drive and delete the .dmg file.
 VOLUME=$(hdiutil attach $filename | grep Volumes | awk '{print $3}')

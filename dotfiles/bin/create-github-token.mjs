adbaf8ff36f82d78155ccd5f751ce7ad1622ef4f
SirWindfield <SirWindfield@users.noreply.github.com>
2021-05-12 03:37:08 +0200

    style: format files

--- dotfiles/bin/create-github-token.mjs
+++ dotfiles/bin/create-github-token.mjs
@@ -1,41 +1,43 @@
 #!/usr/bin/env hs
 
 const scopes = [
-    "repo",
-    "repo:status",
-    "repo_deployment",
-    "public_repo",
-    "repo:invite",
-    "security_events",
+  "repo",
+  "repo:status",
+  "repo_deployment",
+  "public_repo",
+  "repo:invite",
+  "security_events",
 
-    "workflow",
+  "workflow",
 
-    "write:packages",
-    "read:packages",
+  "write:packages",
+  "read:packages",
 
-    "delete:packages",
+  "delete:packages",
 
-    "gist",
+  "gist",
 
-    "notifications",
+  "notifications",
 
-    "delete_repo",
-]
+  "delete_repo",
+];
 const questions = [
-    {
-        type: 'input',
-        name: 'description',
-        message: 'Name of the token',
-    },
-    {
-        type: 'autocomplete',
-        name: 'scopes',
-        message: 'Select access token scopes',
-        choices: scopes,
-        multiple: true,
-    },
-]
+  {
+    type: "input",
+    name: "description",
+    message: "Name of the token",
+  },
+  {
+    type: "autocomplete",
+    name: "scopes",
+    message: "Select access token scopes",
+    choices: scopes,
+    multiple: true,
+  },
+];
 
-const answers = await prompt(questions)
-const url = `https://github.com/settings/tokens/new?description=${answers['description']}&scopes=${answers['scopes'].join(',')}`
-await open(url)
+const answers = await prompt(questions);
+const url = `https://github.com/settings/tokens/new?description=${
+  answers["description"]
+}&scopes=${answers["scopes"].join(",")}`;
+await open(url);

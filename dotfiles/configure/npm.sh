e57faa7472e986870e36ed730d940111ea1c8ef8
SirWindfield <SirWindfield@users.noreply.github.com>
2021-05-12 03:37:34 +0200

    feat: set npm registry for sirwindfield scope

--- dotfiles/configure/npm.sh
+++ dotfiles/configure/npm.sh
@@ -1,3 +1,5 @@
 #!/bin/sh
 
 npm config set prefix '~/.package-managers/npm'
+npm config set @sirwindfield:registry 'https://npm.pkg.github.com'
+npm login --registry 'https://npm.pkg.github.com' --scope=@sirwindfield

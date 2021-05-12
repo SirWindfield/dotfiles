9387bccd8893bd8889217c07366c3c640071b534
SirWindfield <SirWindfield@users.noreply.github.com>
2021-05-12 04:01:45 +0200

    feat: add sudo pre-script authenticator

--- dotfiles/bootstrap.sh
+++ dotfiles/bootstrap.sh
@@ -7,6 +7,10 @@ dir=$(CDPATH= cd -- "$(dirname -- "$0")" && pwd)
 main() {
     # macOS scripts
     if [[ $(uname -s) == "Darwin" ]]; then
+        sudo -v
+        # Keep-alive: update existing `sudo` time stamp until `.osx` has finished
+        while true; do sudo -n true; sleep 60; kill -0 "$$" || exit; done 2>/dev/null &
+
         "$dir/install/vpn.sh"
         "$dir/configure/vpn.sh"
         
